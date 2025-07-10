90e8a9e60924ad6baf1b053f5231b69f9558a018
arjulrao <arjulrao21@gmail.com>
2025-07-10 22:42:03 +0530

    New APIs Add

--- server.js
+++ server.js
@@ -7,15 +7,33 @@ app.use(express.json());
 let all_notes = [];
 
 app.post('/notes', (req, res) => {
-    // console.log(req.body)
     all_notes.push(req.body);
     res.json({
-        Message : "Title and Description add successful",
-        Notes : all_notes
+        Message : "Title and Description add successful"
+    })
+})
+
+app.get('/notes', (req, res) => {
+    res.json(all_notes);
+})
+
+app.delete('/notes/:index', (req, res)=>{
+    const index = req.params.index;
+    all_notes.splice(index-1, 1);
+    res.json({
+        Message : "Note Delete Successful."
+    })
+})
+
+app.patch('/notes/:index', (req, res)=>{
+    const index = req.params.index;
+    const {title} = req.body;
+    all_notes[index-1].title = title;
+    res.json({
+        Message : "Note update successful."
     })
 })
 
 app.listen(3000, ()=> {
     console.log("Server Start.")
 })
-
